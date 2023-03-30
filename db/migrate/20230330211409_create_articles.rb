81ddff7aa55aabc002f55a451ee9aa871607f7b6
AllenJoeG <allen.joeg@gmail.com>
2023-03-30 16:27:30 -0500

    Notes on interacting with db through IRB console

--- db/migrate/20230330211409_create_articles.rb
+++ db/migrate/20230330211409_create_articles.rb
@@ -4,7 +4,17 @@ class CreateArticles < ActiveRecord::Migration[7.0]
       t.string :title
       t.text :body
 
+      # timestamps method defines a 'created_at' and 'updated_at' 
+      # of the following format "2023-03-30 21:20:13.843674"
       t.timestamps
+      
     end
   end
 end
+
+# IRB notation
+# Article.find(1)
+# => <Article id: 1, title: "Hello Rails", body: "I am on Rails!", created_at: "2020-01-18 23:47:30", updated_at: "2020-01-18 23:47:30">
+
+# Article.all
+# => #<ActiveRecord::Relation [#<Article id: 1, title: "Hello Rails", body: "I am on Rails!", created_at: "2020-01-18 23:47:30", updated_at: "2020-01-18 23:47:30">]>
