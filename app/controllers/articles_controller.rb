66a0e867d2c2c98b7c5419a7a7e96c5ccd317d15
AllenJoeG <allen.joeg@gmail.com>
2023-03-30 17:37:37 -0500

    resources to spin up rest of routes.rb for article. Added placeholder controllers for new, create, show. Changed Index view to link to specific posts. Created Show view.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,25 @@
 class ArticlesController < ApplicationController
+# we're going to manually spin these up rather than do the rails autocreate like we did in Ruby-OdinProject1
+
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "filler title" body: "lorem ipsum")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   #manual declaration per tutorial
-  get "/articles", to:"articles#index"
+  # get "/articles", to:"articles#index"
+
+  # declares all, but will error on navigation attempt until there is a view corresponding for each of the seven RESTful default actions
+  resources :articles
 
   # Defines the root path route ("/")
   root "articles#index"
